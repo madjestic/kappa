4c3e1e1f9b27b951c2f53c761293b19d8964138f
madjestic <madjestic13@gmail.com>
2013-12-23 04:29:12 +0100

    new emacs configs

---
+++ .emacs.d/ecb.el
@@ -0,0 +1,7 @@
+(require 'ecb)
+
+(setq ecb-tip-of-the-day nil)
+
+;;(setq ecb-layout-name "leftSpeedbarHistory02")
+
+(ecb-activate)
---
+++ .emacs.d/elpa/ggtags-20131202.1830/ggtags-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ggtags" "20131202.1830" "GNU Global source code tagging system" (quote ((emacs "24"))))
